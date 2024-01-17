b8deae11fc5265a44d8fa09ae827220b0a85951e
Arthur Batista <64083681+Arthur-Batista@users.noreply.github.com>
2024-01-17 10:48:38 -0300

    Update botao.js

--- botao.js
+++ botao.js
@@ -2,4 +2,4 @@ let x=10;
 let valor=4;
 let a=x++//printa e dps soma, portanto, 10  pirnt e soma 11
 let b=++x//11+1=12 soma e dps printa
-console.log(a,b); //primeiro somou e dps printou
+console.log(a,b); //primeiro somou e dps printo
