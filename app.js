f8843f05a1df32ce37e97635cc31cd9db30afd69
Arthur Batista <64083681+Arthur-Batista@users.noreply.github.com>
2024-01-17 14:20:48 +0000

    Adicionar mais palavras e temas; aleatorizar palavras e temas.

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
-var palavras = ["girafa", "banana", "espanha"]; // Array de palavras a serem adivinhadas
-var dicas = ["Animal", "Fruta", "País"]; // Array de dicas correspondentes às palavras
+var palavras = ["girafa", "macaco", "cachorro", "gato", "coelho", "banana", "laranja", "melancia", "abacaxi", "morango", "espanha", "brasil", "canada", "mexico", "austria", "carro", "moto", "bicicleta", "barco", "onibus", "javascript", "python", "ruby", "assembly", "java"]; // Array de palavras a serem adivinhadas
+var dicas = ["Animal", "Fruta", "País", "Veículo", "Linguagem de Programação"]; // Array de dicas correspondentes às palavras
 var arr; // Variável que vai conter a palavra atual em formato de array
 var tentativas = 0; // Contador de tentativas erradas
-var indice = 0; // Índice da palavra atual
+var indice = Math.floor(Math.random() * 25);; // Índice da palavra atual
+
+function geradorDeDicas(){
+  if (indice <= 4){
+    return dicas[0]
+  } else if (indice > 4 && indice <= 9) {
+    return dicas[1]
+  } else if (indice > 9 && indice <= 14) {
+    return dicas[2]
+  } else if (indice > 14 && indice <= 19) {
+    return dicas[3]
+  } else {
+    return dicas[4]
+  }
+}
 
 function iniciarJogo() { // Função para iniciar o jogo
-    arr = new Array(palavras[indice].length).fill("_"); // Inicializa o array com sublinhados do tamanho da palavra atual
-    document.getElementById("palavra").innerHTML = arr.join(" "); // Exibe a palavra atual (como sublinhados) na página
-    document.getElementById("dica").innerHTML = dicas[indice]; // Exibe a dica atual na página
+  console.log(geradorDeDicas());
+  console.log(indice);
+  arr = new Array(palavras[indice].length).fill("_"); // Inicializa o array com sublinhados do tamanho da palavra atual
+  document.getElementById("palavra").innerHTML = arr.join(" "); // Exibe a palavra atual (como sublinhados) na página
+  document.getElementById("dica").innerHTML = geradorDeDicas(); // Exibe a dica atual na página
 }
 
 function jogar() { // Função para jogar
@@ -26,10 +42,10 @@ function jogar() { // Função para jogar
         document.getElementById("letraInput").value = "";  // Limpa o campo de entrada
 
         if (!arr.includes("_")) { // Se todas as letras foram adivinhadas
-            indice++; // Incrementa o índice para a próxima palavra
-            if (indice < palavras.length) { // Se ainda há palavras a serem adivinhadas
-                iniciarJogo(); // Inicia o jogo com a próxima palavra
-            }
+          indice = Math.floor(Math.random() * 25);; // Gera um valor aleatório de 0 a 24
+          if (indice < palavras.length) { // Se ainda há palavras a serem adivinhadas
+              iniciarJogo(); // Inicia o jogo com a próxima palavra
+          }
         }
     }
 }
